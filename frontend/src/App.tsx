f6a0c59051f39dd677ffb2a4e09092a766d5e97b
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-02-19 21:17:30 -0500

    Added Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import Resources from './pages/Resources';
@@ -9,6 +10,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
+      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} /> {/* 404 */}
