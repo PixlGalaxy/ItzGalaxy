9230f6d390073e7c412418f90b0673cbb1ca71a4
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-02-19 20:49:52 -0500

    Updated Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,33 +7,33 @@ const Navbar: React.FC = () => {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-gray-900 bg-opacity-95 shadow-sm backdrop-blur-sm z-50 navbar-color" style={{ fontFamily: 'Oswald-Bold' }}>
+    <nav className="fixed top-0 left-0 right-0 shadow-sm backdrop-blur-sm z-50 navbar-color" style={{ fontFamily: 'Oswald-Bold' }}>
       <div className="mx-auto px-4 py-4 flex items-center justify-between">
 
         {/* Logo and Label */}
         <div className="flex items-center space-x-4">
           <Link to="/" className="logo w-10 h-10 flex-shrink-0">
-            <img src="ItzGalaxy_CLR.png" alt="Logo"></img>
+            <img src="ItzGalaxy_BW.png" alt="Logo"></img>
           </Link>
-          <span className="text-gray-300"><strong>ItzGalaxy.com</strong></span>
+          <span className="text-blck-300"><strong>ItzGalaxy.com</strong></span>
         </div>
 
         {/* Desktop Navigation Links */}
         <div className="hidden md:flex items-center space-x-6">
-          <Link to="/" className="text-gray-300 hover:text-blue-500 transition-colors">
+          <Link to="/" className="text-black-300 hover:text-blue-500 transition-colors">
             <strong>Home</strong>
           </Link>
-          <Link to="/resources" className="text-gray-300 hover:text-blue-500 transition-colors">
+          <Link to="/resources" className="text-black-300 hover:text-blue-500 transition-colors">
             <strong>Resources</strong>
           </Link>
-          <a href="https://onlinestatus.itzgalaxy.com" className="text-gray-300 hover:text-blue-500 transition-colors">
+          <a href="https://onlinestatus.itzgalaxy.com" className="text-black-300 hover:text-blue-500 transition-colors">
             <strong>Server Status</strong>
           </a>
         </div>
 
         {/* Mobile Hamburger */}
         <div className="flex md:hidden items-center">
-          <button onClick={toggleMenu} className="text-gray-300 hover:text-blue-500">
+          <button onClick={toggleMenu} className="text-black-300 hover:text-black-500">
             {isMenuOpen ? (
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -50,13 +50,13 @@ const Navbar: React.FC = () => {
       {/* Mobile Menu */}
       <div className={`navbar-mobile ${isMenuOpen ? 'active' : ''}`}>
         <div className="flex flex-col space-y-3 text-center">
-          <Link to="/" onClick={() => setIsMenuOpen(false)} className="navbar-link text-gray-300 hover:text-blue-500 transition-colors">
+          <Link to="/" onClick={() => setIsMenuOpen(false)} className="navbar-link text-black-300 hover:text-blue-500 transition-colors">
             <strong>Home</strong>
           </Link>
-          <Link to="/resources" onClick={() => setIsMenuOpen(false)} className="navbar-link text-gray-300 hover:text-blue-500 transition-colors">
+          <Link to="/resources" onClick={() => setIsMenuOpen(false)} className="navbar-link text-black-300 hover:text-blue-500 transition-colors">
             <strong>Resources</strong>
           </Link>
-          <a href="https://onlinestatus.itzgalaxy.com" className="navbar-link text-gray-300 hover:text-blue-500 transition-colors">
+          <a href="https://onlinestatus.itzgalaxy.com" className="navbar-link text-black-300 hover:text-blue-500 transition-colors">
             <strong>Server Status</strong>
           </a>
         </div>
