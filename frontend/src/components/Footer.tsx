3ec7342e47e02075e6c28e8828636e9d36631b09
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-04-06 22:14:13 -0400

    chore: fixed footer overlay issue

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full text-black text-left px-4 py-2 z-50 shadow-md">
+    <footer className="text-black text-left px-4 py-4 z-10">
       <p>
         &copy; {new Date().getFullYear()} ItzGalaxy.com | Itz Galaxy Web Services | All Rights Reserved.
       </p>
