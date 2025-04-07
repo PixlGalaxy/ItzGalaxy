d11abb2527de4cb053f6e61c6e5b593782dfeb81
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-04-06 20:33:09 -0400

    chore: update footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="ttext-black text-center md:text-left px-4">
-        <p>
-            &copy; {new Date().getFullYear()} ItzGalaxy.com | Itz Galaxy Web Services | All Rights Reserved.
-        </p>
+    <footer className="fixed bottom-0 left-0 w-full text-black text-left px-4 py-2 z-50 shadow-md">
+      <p>
+        &copy; {new Date().getFullYear()} ItzGalaxy.com | Itz Galaxy Web Services | All Rights Reserved.
+      </p>
     </footer>
   );
-}
+};
 
 export default Footer;
