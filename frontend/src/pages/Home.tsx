2e134b19d63a0863f1c1d1c6d293def747df0df4
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-04-07 17:22:16 -0400

    chore: removed dev section on home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,19 +7,6 @@ const Home: React.FC = () => {
       <title>ItzGalaxy - Home</title>
       <main className="flex-1">
 
-        {/* Dev Page Section */}
-        <section
-          className="relative text-white py-12 px-4 pt-20 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: 'url("/backgrounds/RedBackground.png")' }}
-        >
-          <div className="absolute inset-0 bg-white opacity-10 z-0" />
-          <div className="relative z-10 container mx-auto flex flex-col items-center justify-center text-center space-y-6">
-            <LucideIcons.AlertTriangle className="w-16 h-16 text-yellow-400" strokeWidth={2} />
-            <h2 className="text-2xl md:text-4xl font-bold">This page is still under development.</h2>
-            <h1>Some pages may not work as expected.</h1>
-          </div>
-        </section>
-
         {/* ItzGalaxy Title Section */}
         <section
           className="relative text-white py-12 px-4 pt-20 bg-cover bg-center bg-no-repeat"
